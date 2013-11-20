39f7a633ece960a5e91fb39f80dad2ec1e5f23a0
bunsman <jq1260@126.com>
2013-11-20 19:47:53 +0800

    Update UIButton+UIButtonImageWithLable.h

--- UIButton+UIButtonImageWithLable.h
+++ UIButton+UIButtonImageWithLable.h
@@ -1,3 +1,13 @@
+//
+//  UIButton+UIButtonImageWithLable.h
+//  RESwitchExample
+//
+//  Created by bunsman on 13-11-20.
+//  Copyright (c) 2013年 Bunsman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
 @interface UIButton (UIButtonImageWithLable)
 
 - (void) setImage:(UIImage *)image withHorizontalTitle:(NSString *)title leftTitle:(BOOL)left forState:(UIControlState)stateType ;
