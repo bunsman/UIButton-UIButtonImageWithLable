06a2d62211d142e0f591c3fe9e4b9fd7c54eb90f
bunsman <jq1260@126.com>
2013-11-20 19:47:30 +0800

    Create UIButton+UIButtonImageWithLable.m

---
+++ UIButton+UIButtonImageWithLable.m
@@ -0,0 +1,44 @@
+//
+//  UIButton+UIButtonImageWithLable.m
+//  RESwitchExample
+//
+//  Created by bunsman on 13-11-20.
+//  Copyright (c) 2013年 Bunsman. All rights reserved.
+//
+
+#import "UIButton+UIButtonImageWithLable.h"
+
+@implementation UIButton (UIButtonImageWithLable)
+
+//UIEdgeInsetsMake(CGFloat top, CGFloat left, CGFloat bottom, CGFloat right)
+- (void) setImage:(UIImage *)image withHorizontalTitle:(NSString *)title leftTitle:(BOOL)left forState:(UIControlState)stateType
+{
+    [self.imageView setContentMode:UIViewContentModeCenter];
+    [self setImage:image forState:stateType];
+    
+    [self.titleLabel setContentMode:UIViewContentModeCenter];
+    CGSize titleSize = [title sizeWithFont:self.titleLabel.font];
+    UIEdgeInsets inset = UIEdgeInsetsMake(0.0, 0.0, 0.0, -titleSize.width);
+    if (left) {
+        inset = UIEdgeInsetsMake(0.0, -image.size.width * 2 - titleSize.width, 0.0, 0.0);
+    }
+    [self setTitleEdgeInsets:inset];
+    [self setTitle:title forState:stateType];
+}
+
+
+- (void) setImage:(UIImage *)image withVerticalTitle:(NSString *)title topTitle:(BOOL)top  forState:(UIControlState)stateType
+{
+    [self.imageView setContentMode:UIViewContentModeCenter];
+    [self setImage:image forState:stateType];
+    
+    [self.titleLabel setContentMode:UIViewContentModeCenter];
+    UIEdgeInsets inset = UIEdgeInsetsMake(image.size.height, -image.size.width, -image.size.height, 0.0);
+    if (top) {
+        inset = UIEdgeInsetsMake(-image.size.height, -image.size.width, image.size.height, 0.0);
+    }
+    [self setTitleEdgeInsets:inset];
+    [self setTitle:title forState:stateType];
+}
+
+@end
